fc049d4db425b4a4251a280ab08eb2b829f95b2d
IKU <IKU@MacBook-Pro.local>
2021-12-30 16:13:17 +0900

    remove nextPage

--- pages/Contact/Contact.tsx
+++ pages/Contact/Contact.tsx
@@ -1,10 +1,10 @@
-import { NextPage } from "next";
+//import { NextPage } from "next";
 import Layout from "../layout/Layout";
 import style from "contact/contact.module.scss"
 
 
 
-const Contact: NextPage = () => {
+const Contact = () => {
   return (
     <Layout title="Kana's Portfolio| contact" className={ style.contact }>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBUlDxo6zq0qkOn8vxDjpsOWAZ2bHuXBnHNVJzxgMYapZY6Q/viewform?embedded=true">読み込んでいます…</iframe>
