36c32cf9ff961c40da4a9538585198c3ef3bc193
IKU <IKU@MacBook-Pro.local>
2021-12-30 15:58:04 +0900

    remove nextPage

--- pages/component/Button.tsx
+++ pages/component/Button.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+//import { NextPage } from "next";
 import Link from "next/link";
 import style from "components.module.scss"
 
@@ -10,7 +10,7 @@ type props = {
   className: string,
 }
 
-const Button: NextPage<props> = (props) => {
+const Button = (props:props) => {
   return (
     <div className={`${style.btn} ${style[props.className]}`} >
       <Link  href={ props.href }>
